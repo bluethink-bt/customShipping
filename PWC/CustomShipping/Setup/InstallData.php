cf5c9fe8839d06eb3db4c602d237c1f7c16c4907
Pramod Gupta <pramodgupta.bt@gmail.com>
2022-02-22 14:27:17 +0530

    Custom Shipping Module

---
+++ PWC/CustomShipping/Setup/InstallData.php
@@ -0,0 +1,65 @@
+<?php
+namespace PWC\CustomShipping\Setup;
+
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory;
+use Magento\Catalog\Model\ResourceModel\Product\Action;
+
+class InstallData implements InstallDataInterface
+{
+	 /** @var EavSetupFactory  */
+	 private $eavSetupFactory;
+
+	 /** @var CollectionFactory  */
+	 private $collectionFactory;
+ 
+	 /** @var Action  */
+	 private $action;
+
+	public function __construct(
+        EavSetupFactory $eavSetupFactory,
+        CollectionFactory $collectionFactory,
+        Action $action
+    ) {
+        $this->eavSetupFactory = $eavSetupFactory;
+        $this->collectionFactory = $collectionFactory;
+        $this->action = $action;
+    }
+	
+	public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+	{
+		$eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+		$eavSetup->addAttribute(
+			\Magento\Catalog\Model\Product::ENTITY,
+			'product_shipping_type',
+			[
+				'type' => 'text',
+				'backend' => '',
+				'frontend' => '',
+				'label' => 'Product Shipping Type',
+				'input' => 'select',
+				'class' => '',
+				'source' => 'PWC\CustomShipping\Model\Options',
+				'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_GLOBAL,
+				'visible' => true,
+				'required' => false,
+				'user_defined' => false,
+				'default' => '0',
+				'searchable' => false,
+				'filterable' => false,
+				'comparable' => false,
+				'visible_on_front' => false,
+				'used_in_product_listing' => true,
+				'unique' => false,
+				'apply_to' => ''
+			]
+		);
+		//set default value for the new custom attribute
+        $productIds = $this->collectionFactory->create()->getAllIds();
+        $this->action->updateAttributes($productIds, ['product_shipping_type' => 0], 0);
+	}
+}
